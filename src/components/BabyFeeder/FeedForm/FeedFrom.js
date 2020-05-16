74ad2ee82809b730932e9312b057b8b0a4fb8987
Tony <tony.le@whitbread.com>
2020-05-16 17:31:12 +0100

    date and time picker added

--- src/components/BabyFeeder/FeedForm/FeedFrom.js
+++ src/components/BabyFeeder/FeedForm/FeedFrom.js
@@ -6,49 +6,75 @@ import Aux from '../../../hoc/Aux/Aux';
 import Button from '../..//UI/Button/Button';
 import './FeedForm.scss';
 import axios from 'axios';
-import DatePicker from 'react-datepicker';
+import Datepicker from 'react-datepicker';
 import '../../UI/Datepicker/Datepicker.scss';
 
 class FeedForm extends Component {
+
     state = {
         date: new Date(),
-        time: '',
+        time: new Date(),
         amount: 10,
         notes: ''
     };
 
-    handleChange = date => {
+    formatDate = (newDate) => {
+        newDate = this.state.date.toJSON().slice(0, 10);
+
+        return newDate.replace(/-/g, '/').split('/').reverse().join('/');
+    }
+
+    handleDateChange = (date) => {
         this.setState({
             date: date
         });
     };
 
+    handleTimeChange = (time) => {
+        this.setState({
+            time: time
+        });
+    };
+
     postDataHandler = () => {
         const feed = {
-            date: this.state.date.toISOString(),
-            time: this.state.time,
+            date: this.state.date.toLocaleDateString(),
+            time: this.state.time.toLocaleTimeString().slice(0, 5),
             amount: this.state.amount,
             notes: this.state.notes
         };
 
         axios.post('https://baby-feeder-uat-185a3.firebaseio.com/feeds.json', feed)
-        .then(response=> {
+        .then(response => {
             console.log(response);
+            this.setState({
+                date: new Date(),
+                time: new Date(),
+                amount: 10,
+                notes: ''
+            })
         })
         .catch(error => {
             console.log(error);
         })
     }
 
-
     render() {
         return (
             <Aux>
-                <DatePicker selected={this.state.date} onChange={this.handleChange} />
-                {/* <Input label="Date" value={this.state.date} change={(event) => this.setState({date: event.target.value})}/>            */}
-                <Input label="Time" value={this.state.time} change={(event) => this.setState({time: event.target.value })}/>
+                <label>Date:</label>
+                <Datepicker selected={this.state.date} onChange={this.handleDateChange} dateFormat="dd/MM/yyyy" />
+                <label>Time:</label>
+                <Datepicker
+                    selected={this.state.time} onChange={this.handleTimeChange}
+                    showTimeSelect
+                    showTimeSelectOnly
+                    timeIntervals={5}
+                    timeCaption="Time"
+                    dateFormat="h:mm aa"
+                />
                 <Dropdown label='Amount' value={this.state.amount} change={(event) => this.setState({ amount: event.target.value })}/>
-                <Textarea label='Notes' value={this.state.notes} change={(event) => this.setState({notes: event.target.value })}/>
+                <Textarea label='Notes' change={(event) => this.setState({notes: event.target.value })}/>
                 <div className="FeedForm--button-container">                
                     <Button styleName="cancel" label="Cancel" />
                     <Button clicked={this.postDataHandler} styleName="" label="Submit"/>
